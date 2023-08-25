2d4235eedf8b2b449b92bca633e564464d538d2d
Elizarova Marina <seagirl1110@gmail.com>
2023-08-25 15:33:46 +0300

    refactor: move data and methods from AppTodoList to App

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <AppFilters />
 
   <main class="app-main">
-    <AppTodoList />
+    <AppTodoList :todos="todos" @toggle-todo="toggleTodo" @remove-todo="removeTodo"/>
 
     <AppAddTodo />
   </main>
@@ -19,6 +19,11 @@ import AppFilters from "./components/AppFilters.vue";
 import AppTodoList from "./components/AppTodoList.vue";
 import AppAddTodo from "./components/AppAddTodo.vue";
 import AppFooter from "./components/AppFooter.vue";
+import { Todo } from "./types/Todo";
+
+interface State {
+  todos: Todo[]
+}
 
 export default defineComponent({
   components: {
@@ -27,6 +32,26 @@ export default defineComponent({
     AppTodoList,
     AppAddTodo,
     AppFooter,
+  },
+  data(): State {
+    return {
+      todos: [
+        { id: 0, text: 'Learn the basics of Vue', completed: true },
+        { id: 1, text: 'Learn the basics of Typescript', completed: false },
+        { id: 2, text: 'Subscribe to the channel', completed: false },
+      ]
+    }
+  },
+  methods: {
+    toggleTodo(id: number) {
+      const targetTodo = this.todos.find(todo => todo.id === id);
+      if (targetTodo) {
+        targetTodo.completed = !targetTodo.completed
+      }
+    },
+    removeTodo(id: number) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    }
   }
 })
 </script>
